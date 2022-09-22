f254cdf990767a2a7db878aead40672617c27fe3
Volodia P.-G <volodia.parol-guarino@proton.me>
2022-09-22 13:43:11 +0200

    Try to avoid autosuspend

--- modules/common.nix
+++ modules/common.nix
@@ -96,6 +96,18 @@
     passwordAuthentication = false;
   };
 
+  security.polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+          if (action.id == "org.freedesktop.login1.suspend" ||
+              action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+              action.id == "org.freedesktop.login1.hibernate" ||
+              action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+          {
+              return polkit.Result.NO;
+          }
+      });
+    '';
+
   # Add one immutable user.
   users.mutableUsers = false;
   users.users.volodia = {
