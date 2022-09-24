e39a6472d8659f9364b266cae1f6831dae1216b2
Volodia P.-G <volodia.parol-guarino@proton.me>
2022-09-24 16:57:44 +0200

    Reinstate substituters

--- flake.nix
+++ flake.nix
@@ -17,17 +17,17 @@
 {
   description = "Volodia P.-G'.s system config";
 
-  # nixConfig = {
-  #   substituters = [
-  #     "https://cache.nixos.org"
-  #     "https://nix-community.cachix.org"
-  #   ];
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
 
-  #   trusted-public-keys = [
-  #     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-  #     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  #   ];
-  # };
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
