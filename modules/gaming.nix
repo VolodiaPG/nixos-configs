5843cd65b1ca26b721a9bb162eba29dfb05fbaab
Volodia P.-G <volodia.parol-guarino@proton.me>
2022-09-24 16:21:22 +0200

    Update popcorntime to use the nixpkgs version

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -6,7 +6,7 @@
   environment.systemPackages = with pkgs; [
     # Steam
     (steam.override { extraPkgs = pkgs: [ mono gtk3 gtk3-x11 libgdiplus zlib ]; }).run
-    (pkgs.callPackage ../pkgs/popcorntime { })
+    popcorntime
     qbittorrent
   ];
 
