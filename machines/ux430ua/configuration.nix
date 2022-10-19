e8520a7f7b3d746af1382c2eda0c7359d9bab6fe
Volodia P.-G <volodia.parol-guarino@proton.me>
2022-10-19 16:39:32 +0200

    Remove useless harware accel for an intell hd620

--- machines/ux430ua/configuration.nix
+++ machines/ux430ua/configuration.nix
@@ -47,9 +47,9 @@
     enable = true;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      libvdpau-va-gl
+      # vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      # vaapiVdpau
+      # libvdpau-va-gl
     ];
   };
 
