f3e585e3cb6ce919f17fd490fd736dc9f56d5972
Volodia P.-G <volodia.parol-guarino@proton.me>
2022-10-23 14:21:28 +0200

    Remove scripts

--- scripts/apply-system-ux430ua.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -euo pipefail
-
-pushd $(readlink -f "$(dirname $(readlink -f "$0"))/..")
-sudo nixos-rebuild switch --flake .#ux430ua
--- scripts/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -euo pipefail
-
-pushd $(readlink -f "$(dirname $(readlink -f "$0"))/..")
-nix flake update
-
