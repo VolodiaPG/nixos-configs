25288802531afe11dc535831c77aa8b11942fbea
Volodia P.-G <volodia.parol-guarino@proton.me>
2022-11-21 09:43:58 +0100

    Add zoom and obs studio

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,11 +1,15 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     # Steam
-    (steam.override { extraPkgs = pkgs: [ mono gtk3 gtk3-x11 libgdiplus zlib ]; }).run
+    (steam.override { extraPkgs = _: [ mono gtk3 gtk3-x11 libgdiplus zlib ]; }).run
     popcorntime
     qbittorrent
+    obs-studio
+    obs-studio-plugins.wlrobs
+    obs-studio-plugins.obs-pipewire-audio-capture
+    zoom-us
   ];
 
   programs.steam.enable = true;
