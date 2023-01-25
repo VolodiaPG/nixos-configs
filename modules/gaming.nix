66e3973b0458472f22ac204ea1ef376cb72a180c
Volodia P.-G <volodia.parol-guarino@proton.me>
2023-01-25 17:40:27 +0100

    chore: Remove unused software

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -6,10 +6,10 @@
     (steam.override { extraPkgs = _: [ mono gtk3 gtk3-x11 libgdiplus zlib ]; }).run
     popcorntime
     qbittorrent
-    obs-studio
-    obs-studio-plugins.wlrobs
-    obs-studio-plugins.obs-pipewire-audio-capture
-    zoom-us
+    # obs-studio
+    # obs-studio-plugins.wlrobs
+    # obs-studio-plugins.obs-pipewire-audio-capture
+    # zoom-us
   ];
 
   programs.steam.enable = true;
