8519e5cedee181558d9bef4fc04e97e0d0202d03
Volodia P.-G <volodia.parol-guarino@proton.me>
2023-03-05 13:10:55 +0100

    fix(play-with-mpv): Fix PATH

--- users/volodia/home.nix
+++ users/volodia/home.nix
@@ -90,16 +90,17 @@
 
   systemd.user.services = {
     play-with-mpv = {
-      Service = {
-        Requires = ["xdg-desktop-autostart.target"];
+      serviceConfig = {
         ExecStart = let
           script = pkgs.writeShellScript "play-with-mpv-script" ''
+            PATH=$PATH:${pkgs-unstable.mpv}/bin
             ${pkgs.systemd}/share/factory/etc/X11/xinit/xinitrc.d/50-systemd-user.sh
             ${pkgs.play-with-mpv}/bin/play-with-mpv
           '';
         in "${script}";
       };
       Install = {
+        Requires = ["xdg-desktop-autostart.target"];
         WantedBy = ["default.target"];
       };
     };
