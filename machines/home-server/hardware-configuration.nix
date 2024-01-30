a113fb0e41b5807d0113f3438fa6443bf027ef1f
Volodia P.-G. <volodia.parol-guarino@proton.me>
2024-01-30 23:04:06 +0100

    fix: remove duplicated because of disko

--- machines/home-server/hardware-configuration.nix
+++ machines/home-server/hardware-configuration.nix
@@ -13,32 +13,12 @@
 
   boot = {
     initrd = {
-      availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "sd_mod"];
+      availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
       kernelModules = ["dm-snapshot"];
     };
     kernelModules = ["kvm-intel"];
     extraModulePackages = [];
   };
-
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-uuid/DA73-7E25";
-      fsType = "vfat";
-    };
-
-    "/private" = {
-      device = "/dev/${config.services.impermanence.rootVolume}";
-      fsType = "btrfs";
-      options = ["subvol=private" "ssd" "compress-force=zstd:15" "noatime" "discard=async" "space_cache=v2" "autodefrag"];
-    };
-  };
-
-  swapDevices = [
-    {
-      device = "/dev/disk/by-uuid/78410e51-556c-451f-829e-eddf32c9b315";
-      options = ["noatime"];
-    }
-  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
