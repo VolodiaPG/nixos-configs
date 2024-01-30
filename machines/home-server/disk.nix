bbc918859d6abc4bb08a1689100b4a03af4a521c
Volodia P.-G. <volodia.parol-guarino@proton.me>
2024-01-30 22:02:28 +0100

    fix: fix disk name for disko

--- machines/home-server/disk.nix
+++ machines/home-server/disk.nix
@@ -3,7 +3,7 @@
     disk = {
       vdb = {
         type = "disk";
-        device = "/dev/disk/by-diskseq/1";
+        device = "/dev/sda";
         content = {
           type = "gpt";
           partitions = {
