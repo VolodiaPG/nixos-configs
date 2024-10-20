fc9505dc19edd55cc9bfe360eec2327505ac978f
Volodia P.-G. <volodia.parol-guarino@proton.me>
2024-10-20 12:42:35 +0200

    refactor: use official cursor binary from nix instead of always the flake oen

--- users/volodia/packages/personal.nix
+++ users/volodia/packages/personal.nix
@@ -1,26 +1,33 @@
 {
   pkgs,
-  inputs,
   lib,
+  inputs,
   ...
 }: {
-  home.packages =
-    (lib.optional pkgs.stdenv.isx86_64 ./x68_64.nix)
-    ++ (
-      with pkgs; [
-        inputs.codecursor.packages.${pkgs.stdenv.system}.default
-        freecad
-        inkscape
+  home.packages = with pkgs;
+    [
+      freecad
+      inkscape
 
-        # Media
-        tidal-hifi
-        libsForQt5.qt5.qtwayland # Allow SVP to run on wayland
-        # mpv
-        vlc
-        signal-desktop
-        qbittorrent
-      ]
-    );
+      # Media
+      tidal-hifi
+      libsForQt5.qt5.qtwayland # Allow SVP to run on wayland
+      # mpv
+      vlc
+      signal-desktop
+      qbittorrent
+    ]
+    ++ (lib.optionals pkgs.stdenv.isx86_64 [
+      code-cursor
+      discord
+      insomnia
+      inputs.zen-browser.packages.${pkgs.stdenv.system}.specific
+      (steam.override {extraPkgs = _: [mono gtk3 gtk3-x11 libgdiplus zlib];}).run
+      popcorntime
+    ])
+    ++ (lib.optionals pkgs.stdenv.isAarch64 [
+      inputs.codecursor.packages.${pkgs.stdenv.system}.default
+    ]);
 
   # programs.steam.enable = true;
 }
