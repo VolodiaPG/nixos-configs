0fa53d3334e86f77ace69b15a1f2943ec9e5e061
Volodia P.-G. <volodia.parol-guarino@proton.me>
2024-12-30 11:47:29 +0100

    feat: use volodia as a user instead of root

--- flake.nix
+++ flake.nix
@@ -523,7 +523,7 @@
             dell = {
               hostname = "dell";
               profiles.system = {
-                user = "root";
+                user = "volodia";
                 sshUser = "volodia";
                 path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.dell;
               };
