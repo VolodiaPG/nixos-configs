1678871284fecd825152a3c5f938a842eb4339ed
Volodia P.-G. <volodia.parol-guarino@proton.me>
2025-01-19 11:52:32 +0100

    feat: make synchting not declarative

--- users/volodia/syncthing.nix
+++ users/volodia/syncthing.nix
@@ -4,18 +4,19 @@
   pkgs,
   hostName,
   ...
-}: let
-  msi = "msi";
-  dell = "dell";
-  volodias-macbook-pro = "Volodias-MacBook-Pro";
-  home-server = "home-server";
-  allDevices = [volodias-macbook-pro dell msi home-server];
-
-  genDevice = hostName: id: {
-    inherit id;
-    addresses = ["tcp://${hostName}:22000"];
-  };
-in {
+}:
+# let
+# msi = "msi";
+# dell = "dell";
+# volodias-macbook-pro = "Volodias-MacBook-Pro";
+# home-server = "home-server";
+# allDevices = [volodias-macbook-pro dell msi home-server];
+# genDevice = hostName: id: {
+#   inherit id;
+#   addresses = ["tcp://${hostName}:22000"];
+# };
+# in
+{
   services.syncthing = {
     enable = true;
     # openDefaultPorts = true;
@@ -27,19 +28,21 @@ in {
     key = config.sops.secrets."syncthing-${hostName}-key".path;
 
     settings = {
-      devices = {
-        ${volodias-macbook-pro} = genDevice volodias-macbook-pro "WLVZHQK-QBOS6XW-5I6WIPH-VZTUSHQ-CA7QWCW-TQ5VT2G-7WD3MC7-ANZDEQQ";
-        ${dell} = genDevice dell "UFAZWAJ-4BFYDEK-MEZ7VPS-AV3YVG2-HDHXKOJ-SZKIBGM-ORDZ77R-NWYFZQB";
-        ${msi} = genDevice msi "OPL2OSX-P5EIM6J-OIIKMZI-QE7IEXU-2KURAEU-KBWNUI7-CBWQK4H-CJQK2AO";
-        ${home-server} = genDevice home-server "S3JD5BW-PMNUD5Y-GGRJINH-FK7MS5B-KEKJJDT-AM6Y6SA-WFAK2M5-S2N65AL";
-      };
+      # devices = {
+      #   ${volodias-macbook-pro} = genDevice volodias-macbook-pro "WLVZHQK-QBOS6XW-5I6WIPH-VZTUSHQ-CA7QWCW-TQ5VT2G-7WD3MC7-ANZDEQQ";
+      #   ${dell} = genDevice dell "UFAZWAJ-4BFYDEK-MEZ7VPS-AV3YVG2-HDHXKOJ-SZKIBGM-ORDZ77R-NWYFZQB";
+      #   ${msi} = genDevice msi "OPL2OSX-P5EIM6J-OIIKMZI-QE7IEXU-2KURAEU-KBWNUI7-CBWQK4H-CJQK2AO";
+      #   ${home-server} = genDevice home-server "S3JD5BW-PMNUD5Y-GGRJINH-FK7MS5B-KEKJJDT-AM6Y6SA-WFAK2M5-S2N65AL";
+      # };
+
+      overrideDevices = false;
       folders = {
         "Sync" = {
           path =
             if lib.strings.hasSuffix "linux" pkgs.system
             then "/home/volodia/Documents/Sync"
             else "/Users/volodia/Documents/Sync";
-          devices = allDevices;
+          # devices = allDevices;
           # By default, Syncthing doesn't sync file permissions. This line enables it for this folder.
           ignorePerms = false;
         };
