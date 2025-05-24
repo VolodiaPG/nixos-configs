bc770524e56a600fe61132b6e62b2306fc035cf2
Volodia P.-G. <volodia.parol-guarino@proton.me>
2025-05-24 21:38:50 +0200

    fix: try fix syncthing by removing nix from overriding the same default dir each time

--- users/volodia/syncthing.nix
+++ users/volodia/syncthing.nix
@@ -1,23 +1,8 @@
 {
-  lib,
-  pkgs,
-  ...
-}: {
   services.syncthing = {
     enable = true;
     overrideDevices = false;
     overrideFolders = false;
     guiAddress = "0.0.0.0:8384";
-    settings = {
-      folders = {
-        "Sync" = {
-          path =
-            if lib.strings.hasSuffix "linux" pkgs.system
-            then "~/Documents/Sync"
-            else "/Users/volodia/Documents/Sync";
-          ignorePerms = false;
-        };
-      };
-    };
   };
 }
