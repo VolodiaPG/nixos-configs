3de9a6d639b11dd79faa585bde94b9778fae4683
Volodia P.-G. <volodia.parol-guarino@proton.me>
2025-06-16 18:32:36 +0200

    feat: clean up personal packages and add aider chat

--- users/volodia/packages/personal.nix
+++ users/volodia/packages/personal.nix
@@ -1,16 +1,8 @@
 {
+  pkgs-unstable,
   pkgs,
-  lib,
-  inputs,
   ...
-}:
-# let
-#   stable-pkgs = import inputs.nixpkgs-stable {
-#     config.allowUnfree = true;
-#     inherit (pkgs.stdenv) system;
-#   };
-# in
-{
+}: {
   # programs.obs-studio = {
   #   enable = true;
   #   plugins = with stable-pkgs.obs-studio-plugins; [
@@ -20,45 +12,14 @@
   #     obs-ndi
   #   ];
   # };
-  home.packages = with pkgs;
-    [
-      # inkscape
-      # signal-desktop
-      # qbittorrent
-      lazygit
-      nixd
-      nil
-      # alejandra
-      # elixir-ls
-      # go
-      # gopls
-      inputs.nixpkgs-unstable.legacyPackages.${pkgs.stdenv.system}.devenv
-      ripgrep
-      nvim
-      opencode
-      # tree-sitter
-      # nodejs
-      # zathura
-    ]
-    ++ (lib.optionals pkgs.stdenv.isLinux [
-      # neovide
-      # vlc
-      # freecad
-      # strawberry
-      # tidal-hifi
-      # # Media
-      # libsForQt5.qt5.qtwayland # Allow SVP to run on wayland
-    ])
-    ++ (lib.optionals (pkgs.stdenv.isx86_64 && pkgs.stdenv.isLinux) [
-      # code-cursor
-      # discord
-      # insomnia
-      # (steam.override {extraPkgs = _: [mono gtk3 gtk3-x11 libgdiplus zlib];}).run
-      # popcorntime
-    ])
-    ++ (lib.optionals (pkgs.stdenv.isAarch64 && pkgs.stdenv.isLinux) [
-      # inputs.codecursor.packages.${pkgs.stdenv.system}.default
-    ]);
+  home.packages = [
+    pkgs.lazygit
+    pkgs.ripgrep
+    pkgs.nvim
+    pkgs.opencode
+    pkgs-unstable.devenv
+    pkgs-unstable.aider-chat
+  ];
 
   # programs.steam.enable = true;
 }
