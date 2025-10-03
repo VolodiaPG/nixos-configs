7de8d728bc64f6a6ccc0d892bfae7d305d5e44fb
Volodia P.-G. <volodia.parol-guarino@proton.me>
2025-10-03 16:14:03 +0200

    feat: add orca slicer and fixes

--- users/volodia/gnome.nix
+++ users/volodia/gnome.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   pkgs-unstable,
   ...
 }:
@@ -40,8 +41,39 @@
     distrobox
     vlc
     nur.repos.Ev357.helium
-    orca-slicer
+    # orca-slicer
   ];
+  xdg.desktopEntries = {
+    OrcaSlicer = {
+      name = "OrcaSlicer";
+      genericName = "3D Printing Software";
+      icon = "OrcaSlicer";
+      exec = lib.concatStringsSep " " [
+        "${pkgs.coreutils}/bin/env"
+        "__GLX_VENDOR_LIBRARY_NAME=mesa"
+        "__EGL_VENDOR_LIBRARY_FILENAMES=${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json"
+        "MESA_LOADER_DRIVER_OVERRIDE=zink"
+        "GALLIUM_DRIVER=zink"
+        "WEBKIT_DISABLE_DMABUF_RENDERER=1"
+        "${lib.getExe pkgs.orca-slicer} %U"
+      ];
+      terminal = false;
+      type = "Application";
+      mimeType = [
+        "model/stl"
+        "model/3mf"
+        "application/vnd.ms-3mfdocument"
+        "application/prs.wavefront-obj"
+        "application/x-amf"
+      ];
+      categories = [
+        "Graphics"
+        "3DGraphics"
+        "Engineering"
+      ];
+      startupNotify = false;
+    };
+  };
   # can use the gnome dump command
   dconf = {
     enable = true;
