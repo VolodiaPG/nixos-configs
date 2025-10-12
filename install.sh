d63dbb7db3addf17b00e516b8205910c9fbc5ef1
Volodia P.-G. <volodia.parol-guarino@proton.me>
2025-10-12 14:20:31 +0200

    feat: add install script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+swapon /dev/disk/by-label/swap
+
+# Then create subvolumes
+mount -t btrfs /dev/disk/by-label/root /mnt
+
+# We first create the subvolumes outlined above:
+btrfs subvolume create /mnt/root
+btrfs subvolume create /mnt/nix
+btrfs subvolume create /mnt/persistent
+
+umount /mnt
+
+# Mount the directories
+#
+OPTS="ssd,compress-force=zstd:2,noatime,discard=async,space_cache=v2,autodefrag"
+
+mount -o subvol=root,$OPTS /dev/disk/by-label/root /mnt
+
+mkdir /mnt/nix
+mount -o subvol=nix,$OPTS /dev/disk/by-label/nix /mnt
+
+mkdir /mnt/persistent
+mount -o subvol=persistent,$OPTS /dev/disk/by-label/persistent /mnt
+
+# don't forget this!
+mkdir /mnt/boot
+mount /dev/disk/by-label/BOOT /mnt/boot
