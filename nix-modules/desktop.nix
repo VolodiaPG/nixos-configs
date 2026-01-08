cde28fa4acedb351b6057940f47586c70796fc72
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-01-08 18:49:42 +0100

    feat: better fonts

--- nix-modules/desktop.nix
+++ nix-modules/desktop.nix
@@ -82,6 +82,12 @@ in
       pkgs.ddcutil
     ];
 
+    environment.variables = {
+      # better fonts:
+      # https://web.archive.org/web/20230921201835/https://old.reddit.com/r/linux_gaming/comments/16lwgnj/is_it_possible_to_improve_font_rendering_on_linux/
+      FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0";
+    };
+
     services.udev.extraRules = ''
       KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
     '';
