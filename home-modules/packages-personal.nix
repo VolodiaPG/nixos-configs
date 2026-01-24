feb1a772304e9caf3ce0601af3dcf6b004a1c7ee
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-01-24 19:38:22 +0100

    fix: disable devenv mods

--- home-modules/packages-personal.nix
+++ home-modules/packages-personal.nix
@@ -17,35 +17,35 @@
     pkgs.cachix
     pkgs.nvim
     inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode
-    # pkgs.devenv
-    (pkgs.devenv.overrideAttrs (_old: {
-      postInstall =
-        let
-          setDefaultLocaleArchive = lib.optionalString (pkgs.glibcLocalesUtf8 != null) ''
-            --set-default LOCALE_ARCHIVE ${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive
-          '';
-        in
-        ''
-          wrapProgram $out/bin/devenv \
-            --prefix PATH ":" "$out/bin:${pkgs.cachix}/bin" \
-            --set DEVENV_NIX ${pkgs.nix}/bin/nix \
-            ${setDefaultLocaleArchive}
-
-          # Generate manpages
-          cargo xtask generate-manpages --out-dir man
-          installManPage man/*
-
-          # Generate shell completions
-          compdir=./completions
-          for shell in bash fish zsh; do
-            cargo xtask generate-shell-completion $shell --out-dir $compdir
-          done
-
-          installShellCompletion --cmd devenv \
-            --bash $compdir/devenv.bash \
-            --fish $compdir/devenv.fish \
-            --zsh $compdir/_devenv
-        '';
-    }))
+    pkgs.devenv
+    # (pkgs.devenv.overrideAttrs (_old: {
+    #   postInstall =
+    #     let
+    #       setDefaultLocaleArchive = lib.optionalString (pkgs.glibcLocalesUtf8 != null) ''
+    #         --set-default LOCALE_ARCHIVE ${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive
+    #       '';
+    #     in
+    #     ''
+    #       wrapProgram $out/bin/devenv \
+    #         --prefix PATH ":" "$out/bin:${pkgs.cachix}/bin" \
+    #         --set DEVENV_NIX ${pkgs.lix}/bin/lix \
+    #         ${setDefaultLocaleArchive}
+    #
+    #       # Generate manpages
+    #       cargo xtask generate-manpages --out-dir man
+    #       installManPage man/*
+    #
+    #       # Generate shell completions
+    #       compdir=./completions
+    #       for shell in bash fish zsh; do
+    #         cargo xtask generate-shell-completion $shell --out-dir $compdir
+    #       done
+    #
+    #       installShellCompletion --cmd devenv \
+    #         --bash $compdir/devenv.bash \
+    #         --fish $compdir/devenv.fish \
+    #         --zsh $compdir/_devenv
+    #     '';
+    # }))
   ];
 }
