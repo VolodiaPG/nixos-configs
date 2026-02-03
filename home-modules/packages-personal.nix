a8187759c52eb138e54b6df706a543f8dbdf16df
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-02-03 14:49:32 +0100

    feat: add opencode plugins

--- home-modules/packages-personal.nix
+++ home-modules/packages-personal.nix
@@ -1,14 +1,27 @@
 {
   pkgs,
+  config,
+  lib,
   ...
 }:
 {
   programs.opencode = {
     enable = true;
-    # settings = {
-    #   "plugin" = [ "@plannotator/opencode@latest" ];
-    # };
+    settings = {
+      "plugin" = [
+        # "@plannotator/opencode@latest"
+        "@simonwjackson/opencode-direnv@latest"
+        "@tarquinen/opencode-dcp@latest"
+        "@knikolov/opencode-plugin-simple-memory@latest"
+      ];
+
+    };
   };
+
+  home.activation.opencode-delete-plugin-cache = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    rm -rf ${config.xdg.configHome}/opencode/node_modules
+  '';
+
   home.packages = [
     pkgs.cachix
     pkgs.nvim
