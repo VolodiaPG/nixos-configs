55397d528f838cdb9a755c69c6b1023c0bc33aed
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-02-04 23:22:10 +0100

    refactor: move config.ssh to assets

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -1,13 +1,17 @@
 {
+  flake,
   pkgs,
   ...
 }:
+let
+  inherit (flake.inputs) self;
+in
 {
   home.file = {
     ".ssh/config" = {
       target = ".ssh/config_source";
       onChange = "cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config";
-      source = pkgs.replaceVars ./config.ssh {
+      source = pkgs.replaceVars (self + "/assets/config.ssh") {
         g5k_login = "volparolguarino";
         keychain = if pkgs.stdenv.isLinux then "" else "UseKeychain yes";
       };
