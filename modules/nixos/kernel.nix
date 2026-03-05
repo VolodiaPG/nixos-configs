f4e60bb3fad06d5ad6b8fa19a4466d98065655e4
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-03-05 11:16:23 +0100

    feat: use conservative sched on battery and default sched on ac

--- modules/nixos/kernel.nix
+++ modules/nixos/kernel.nix
@@ -56,8 +56,8 @@ in
             CPU_BOOST_ON_AC = 1;
             CPU_HWP_DYN_BOOST_ON_AC = 1;
             CPU_HWP_DYN_BOOST_ON_BAT = 0;
-            CPU_SCALING_GOVERNOR_ON_BATTERY = "laputil";
-            CPU_SCALING_GOVERNOR_ON_AC = "laputil";
+            CPU_SCALING_GOVERNOR_ON_BATTERY = "conservative";
+            CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
             CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
             CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
             PLATFORM_PROFILE_ON_AC = "performance";
