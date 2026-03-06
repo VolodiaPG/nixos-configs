68f6e5cc27f8aa111fe8f68b61789f6101f8d9ec
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-03-06 22:14:07 +0100

    chore: add missing key for flakehub cache

--- config.nix
+++ config.nix
@@ -25,6 +25,7 @@
       "volodiapg.cachix.org-1:XcJQeUW+7kWbHEqwzFbwIJ/fLix3mddEYa/kw8XXoRI="
       "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
     ];
   };
 
