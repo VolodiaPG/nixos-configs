6b9441d0d1e08e3c1fc76d089107375684f122b3
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-03-10 11:38:17 +0100

    fix: fix caddy hash mismatch

--- modules/nixos/caddy.nix
+++ modules/nixos/caddy.nix
@@ -18,7 +18,7 @@ in
           plugins = [
             "github.com/tailscale/caddy-tailscale@v0.0.0-20260106222316-bb080c4414ac"
           ];
-          hash = "sha256-9CYQSdGAQwd1cmFuKT2RNzeiJ4DZoyrxvsLS4JDCFCY=";
+          hash = "sha256-bb7yRcm+KXolMdeFFjOXeRBkvcyfUfrTBIOo88gT/FY=";
         };
         globalConfig = ''
           servers {
