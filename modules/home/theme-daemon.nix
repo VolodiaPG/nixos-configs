fae568d574e616de2ba10ad4b7a4d1fd011240c9
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-03-25 10:06:08 +0100

    fix: fix auto darkmode on linux

--- modules/home/theme-daemon.nix
+++ modules/home/theme-daemon.nix
@@ -39,45 +39,24 @@ in
       };
     };
 
-    # Linux: Use darkman
-    systemd.user.services.theme-daemon = lib.mkIf isLinux {
-      Unit = {
-        Description = "Darkman theme daemon";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
+    services.darkman = {
+      enable = isLinux;
+      settings = {
+        usegeoclue = true;
       };
-
-      Service = {
-        Type = "dbus";
-        BusName = "nl.whynothugo.darkman";
-        ExecStart = "${pkgs.darkman}/bin/darkman run";
-        Restart = "on-failure";
-        RestartSec = "5s";
+      darkModeScripts = {
+        theme-switcher = ''
+          ${pkgs.theme-switcher}/bin/theme-switcher dark
+        '';
       };
-
-      Install = {
-        WantedBy = [ "graphical-session.target" ];
+      lightModeScripts = {
+        theme-switcher = ''
+          ${pkgs.theme-switcher}/bin/theme-switcher light
+        '';
       };
     };
 
     home = {
-      # Linux: Darkman configuration
-      file.".config/darkman/light-mode.d/theme-switcher.sh" = lib.mkIf isLinux {
-        executable = true;
-        text = ''
-          #!/usr/bin/env bash
-          ${pkgs.theme-switcher}/bin/theme-switcher light
-        '';
-      };
-
-      file.".config/darkman/dark-mode.d/theme-switcher.sh" = lib.mkIf isLinux {
-        executable = true;
-        text = ''
-          #!/usr/bin/env bash
-          ${pkgs.theme-switcher}/bin/theme-switcher dark
-        '';
-      };
-
       # Create state directory
       activation.createThemeStateDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/.local/state/theme-switcher
