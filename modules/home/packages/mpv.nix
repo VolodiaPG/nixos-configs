196fde6cd8811dbce1ccba69fc0239a4d515ccba
Volodia P.G. <volodia.parol-guarino@proton.me>
2026-08-18 12:05:10 +0200

    chore: update config and cleanup

--- modules/home/packages/mpv.nix
+++ modules/home/packages/mpv.nix
@@ -6,23 +6,6 @@
 }:
 let
   cfg = config.mpv;
-  # inherit (lib) mkEnableOption mkIf;
-  #
-  # # mpv with vapoursynth support enabled (needed for vsrife to load).
-  # mpvVapoursynth = pkgs.mpv-unwrapped.override {
-  #   vapoursynth = true;
-  # };
-  # mpvWrapped = pkgs.symlinkJoin {
-  #   name = "mpv";
-  #   paths = [ mpvVapoursynth pkgs.vapoursynth ];
-  #   buildInputs = [ pkgs.makeWrapper ];
-  #   postBuild = ''
-  #     wrapProgram $out/bin/mpv \
-  #       --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [ pkgs.vapoursynth ]}" \
-  #       --prefix PYTHONPATH : "${pkgs.vsrife}/${pkgs.python3.sitePackages}"
-  #   '';
-  #   meta = mpvVapoursynth.meta // { outputsToInstall = [ "out" ]; }; # ponytail: symlinkJoin has no `man` output; inherited meta would crash buildenv
-  # };
 in
 {
   options = {
@@ -32,33 +15,8 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    # programs.mpv.enable = true;
     home.packages = [
       pkgs.mpv-rife
-      # pkgs.mpv-handler
-      # pkgs.vapoursynth
-      # # pkgs.vapoursynth-mvtools
-      # pkgs.python3Packages.vapoursynth
-      # # pkgs.python3Packages.guessit
-      # (pkgs.writeShellScriptBin "mpv-python" ''
-      #   exec ${
-      #     pkgs.python3.withPackages (
-      #       ps: with ps; [
-      #         guessit
-      #         requests
-      #         subliminal
-      #       ]
-      #     )
-      #   }/bin/python3 "$@"
-      # '')
-      # pkgs.socat
     ];
-
-    #   # file = {
-    #   #   ".config/mpv" = {
-    #   #     source = ./mpv;
-    #   #     recursive = true;
-    #   #   };
-    #   # };
   };
 }
